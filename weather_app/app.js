7e7b6fdd12df4ddd7e32aae51b87dff984f89cbc
basacul <antelo.lucas@outlook.com>
2022-02-03 22:58:01 +0000

    Resolved challenge

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,9 +1,5 @@
 const geocode = require('./utils/geocode.js');
-const access_keyWeatherStack = "4eb0b13f2e2b030bfaf2a69f55f3ede9";
-
-const urlWeatherStack = `http://api.weatherstack.com/current?access_key=${access_keyWeatherStack}&query=`;
-
-
+const forecast = require('./utils/forecast.js');
 
 geocode('Zurich', (error, data) => {
 	// console.log('Error', error); // for testing
@@ -11,3 +7,17 @@ geocode('Zurich', (error, data) => {
 });
 
 
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+forecast(8.54, 47.37861, (error, data) => {
+  // console.log('Error', error); // for testing
+  console.log('Data', data);
+});
---
+++ weather_app/utils/forecast.js
@@ -0,0 +1,39 @@
+const request = require('request');
+const access_keyWeatherStack = "4eb0b13f2e2b030bfaf2a69f55f3ede9";
+const urlWeatherStack = `http://api.weatherstack.com/current?access_key=${access_keyWeatherStack}&query=`;
+
+const forecast = (latitude, longitude, callback) => {
+
+	const url = `http://api.weatherstack.com/current?access_key=4eb0b13f2e2b030bfaf2a69f55f3ede9&query=${latitude},${longitude}&units=m`;
+
+	request({url:url, json: true}, (error, response) => {
+		if(error){
+			callback(error, undefined);
+		}else if(response.body.error){
+			callback(response.body.error, undefined);
+		}else{
+			const { temperature, weather_descriptions, feelslike } = response.body.current;
+
+			callback(undefined,`${weather_descriptions[0]}. It is currently ${temperature}°C out. It feels like ${feelslike}°C out.`);
+		}
+	});
+};
+
+module.exports = forecast;
+/*
+const coordinates = `${center[1]},${center[0]}`;
+			 
+const url2 = `${urlWeatherStack}${coordinates}&units=m`		 
+request({url: url2, json: true}, (error, response2) => {
+	if(error){
+		console.log(error);
+	}else if(response2.body.error){
+		console.log('Something went wrong with weather stack api.');
+	}else{
+
+		const { temperature, weather_descriptions, feelslike } = response2.body.current;
+
+		console.log(`${place_name}. ${weather_descriptions[0]}. It is currently ${temperature}째C out. It feels like ${feelslike}째C out.`);
+	}
+});
+*/
