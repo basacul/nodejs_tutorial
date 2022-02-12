11bf3cc4f8b261eff0641630151340e52ea5ddb1
basacul <antelo.lucas@outlook.com>
2022-02-12 16:34:14 +0000

    Finished the UI of the weather application

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -3,20 +3,33 @@ console.log('Client side javascript file is loaded.');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
 
 weatherForm.addEventListener('submit', (e) => {
 	// prevent reload after submit
 	e.preventDefault();
 	
+	messageOne.textContent = 'Loading....';
+	messageTwo.textContent = '';
+	
 	const location = search.value;
 	const url = `https://nodejs-tutorial-isyzm.run-eu-central1.goorm.io/weather?address=${location}`; 
+	
 	fetch(url).then((response) => {
+		
 		response.json().then((data) => {
+			
 			if(data.error){
-				console.log(data.error);
+					
+				messageOne.textContent = data.error;
+
 			}else{
-				console.log(data.location);	
-				console.log(data.forecast);	
+				
+				messageOne.textContent = data.location;
+				messageTwo.textContent = data.forecast;
+ 
 			}
 		});
 	});
